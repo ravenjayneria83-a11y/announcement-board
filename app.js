c4f69c8a9f23b861f124466855b1a73eb926d964
ravenjayneria83-a11y <ravenjayneria83@gmail.com>
2026-04-26 22:18:27 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function postAnnouncement() {
+// Get the input value
+var message = document.getElementById("messageInput").value;
+
+```
+// Display it in the output box
+document.getElementById("outputBox").innerText = message;
+```
+
+}
